841d0ba6fac8d190199950e2b0666c4c8cb4d578
madhur6922 <madhursharma6922@gmail.com>
2025-10-30 10:23:13 +0530

    Initialize React app with Docker support

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🚀 React App Dockerized with Multi-Stage Build</h1>
+      <p>This React application is containerized using Docker and served via Nginx.</p>
+    </div>
+  );
+}
+
+export default App;
